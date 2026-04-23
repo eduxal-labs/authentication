6161e8d4c7410dc998e39bd0ec5684139bdfbb15
Abdihakim Osman <abdulhakimuthman100@gmail.com>
2026-04-23 21:00:09 +0300

    added tracing for info level logs

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,13 @@ type Result<T, E = Box<dyn std::error::Error + Send + Sync>> = std::result::Resu
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| tracing_subscriber::EnvFilter::new("info")),
+        )
+        .init();
+
     let config = aws_config::load_from_env().await;
     let client = aws_sdk_dynamodb::Client::new(&config);
     let router = routes::router(client);
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,8 +1,11 @@
-use axum::{routing::get, Router};
 use aws_sdk_dynamodb::Client;
+use axum::{Router, routing::get};
 
 pub fn router(client: Client) -> Router {
-    Router::new().with_state(client).route("/health", get(health))
+    Router::new()
+        .route("/health", get(health))
+        // future routes go here, before with_state
+        .with_state(client)
 }
 
 pub async fn health() -> &'static str {

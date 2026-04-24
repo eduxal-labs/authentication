3d01231d5ac74e255f550e4d590b54db13b2315b
Abdihakim Osman <abdulhakimuthman100@gmail.com>
2026-04-24 23:19:45 +0300

    updated the routes to use the new universal config object in a generic way

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,10 +1,9 @@
-use aws_sdk_dynamodb::Client;
 use axum::{Router, routing::get};
 
-pub fn router(client: Client) -> Router {
+pub fn router<T: Send + Sync + Clone + 'static>(config: T) -> Router {
     Router::new()
         .route("/health", get(health))
-        .with_state(client)
+        .with_state(config)
 }
 
 pub async fn health() -> &'static str {
